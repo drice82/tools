06de19b7b1a02b8f6d7b26663544eb23f985219c
root <root@debian.debian>
2018-09-14 15:56:16 +0800

    update

--- checkip.py
+++ checkip.py
@@ -1,41 +1,22 @@
 #!/usr/bin/env python
 
 import socket
-import string
-import math
-import requests
-import json
 import time
-import yaml
-import os
-import sys
-import collections
-import boto3
-from subprocess import Popen, PIPE
 
 def main():
-
-    print(ip_status())
+    print(ip_status('www.baidu.com',8080))
     exit()
-
-def die(msg):
-    log('error', msg)
-    raise Exception(msg)
-
 	
-def ip_status():
-    object_check = ['123.125.115.110', '14.215.177.39', '117.136.190.162']
+def ip_status(addr,port):
     ip_check = 0
-    for i in object_check:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.settimeout(1)
-	try:
-	    s.connect((i, 80))
-	    ip_check=1
-	    break
-	except:
-	    continue
-	s.close()
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(2)
+    try:
+        s.connect((addr, port))
+        ip_check=1
+    except:
+        ip_check=0
+    s.close()
     if ip_check==1:
         return True
     else:
